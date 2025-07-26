a5908f26bfcbab59b7c3c9ea7d65b9807dc4d4b5
lucascatanio <lucascatanio@gmail.com>
2025-07-26 00:32:16 -0300

    fix: habilita auditoria @EnableJpaAuditing

--- src/main/java/br/com/lutani/lutani_lib/LutaniLibApplication.java
+++ src/main/java/br/com/lutani/lutani_lib/LutaniLibApplication.java
@@ -2,8 +2,10 @@ package br.com.lutani.lutani_lib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LutaniLibApplication {
 
 	public static void main(String[] args) {
